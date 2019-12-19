2e9a4979fe672cfa98859d066a8cfdcb7ba6ec44
Mitchell Tuinstra <mitchelltuinstra2000@gmail.com>
2019-12-19 11:41:38 +0100

    admin commands and improved info commands

---
+++ DiscordBotV5/Modules/AdminModule.cs
@@ -0,0 +1,62 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace DiscordBotV5.Modules
+{
+    class AdminModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("kick")]
+        [RequireUserPermission(GuildPermission.KickMembers)]
+        [RequireBotPermission(GuildPermission.KickMembers)]
+        public async Task Kick(IGuildUser user, [Remainder] string reason = "No reason was provided")
+        {
+            await Context.Channel.SendMessageAsync($"User {user.Mention} kicked for \"{reason}\"");
+            await user.KickAsync(reason);
+            IDMChannel dm = await user.GetOrCreateDMChannelAsync();
+            await dm.SendMessageAsync($"You got kicked from {Context.Guild.Name} for \"{reason}\"");
+        }
+
+
+        [Command("ban")]
+        [RequireUserPermission(GuildPermission.KickMembers)]
+        [RequireBotPermission(GuildPermission.KickMembers)]
+        public async Task Ban(IGuildUser user, [Remainder] string reason = "No reason was provided")
+        {
+            await Context.Channel.SendMessageAsync($"User {user.Mention} banned for \"{reason}\"");
+            await user.KickAsync(reason);
+            IDMChannel dm = await user.GetOrCreateDMChannelAsync();
+            await dm.SendMessageAsync($"You got banned from {Context.Guild.Name} for \"{reason}\"");
+        }
+
+        [Command("mute")]
+        [RequireUserPermission(GuildPermission.MuteMembers)]
+        [RequireBotPermission(GuildPermission.MuteMembers)]
+        public async Task Mute(IGuildUser user)
+        {
+            if (!user.IsMuted)
+            {
+                await user.ModifyAsync(x =>
+                {
+                    x.Mute = true;
+                });
+            }
+        }
+
+        [Command("mute")]
+        [RequireUserPermission(GuildPermission.MuteMembers)]
+        [RequireBotPermission(GuildPermission.MuteMembers)]
+        public async Task UnMute(IGuildUser user)
+        {
+            if (user.IsMuted)
+            {
+                await user.ModifyAsync(x =>
+                {
+                    x.Mute = false;
+                });
+            }
+        }
+
+
+    }
+}
--- DiscordBotV5/Modules/InfoModule.cs
+++ DiscordBotV5/Modules/InfoModule.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
+using System.Linq;
 
 namespace DiscordBot.Modules
 {
@@ -15,7 +16,7 @@ namespace DiscordBot.Modules
             embed.WithTitle("Bot Info");
             embed.WithColor(Color.Gold);
             embed.WithDescription($"Hello, My name is {Context.Client.CurrentUser.Username}. I am written in C#(Discord.net) by <@165846891941199872>\n" +
-                "My source code is available at COMING SOON\n");
+                "My source code is available on [Github](https://github.com/MLGC00KIE/DiscordBot)\n");
 
 
             await Context.Channel.SendMessageAsync("", false, embed.Build());
@@ -24,12 +25,18 @@ namespace DiscordBot.Modules
         [Command("serverinfo")]
         public async Task ServerInfo()
         {
+            await Context.Message.DeleteAsync();
+
             var embed = new EmbedBuilder();
             embed.WithAuthor(Context.Guild.Name, Context.Guild.IconUrl);
             embed.WithColor(Color.DarkRed);
             embed.AddField("Owner", Context.Guild.Owner.Mention);
             embed.AddField("Channels", Context.Guild.Channels.Count, true);
+            embed.AddField("Text channels", Context.Guild.TextChannels.Count, true);
+            embed.AddField("Voice channels", Context.Guild.VoiceChannels.Count, true);
+            embed.AddField("Categories", Context.Guild.CategoryChannels.Count);
             embed.AddField("Members", Context.Guild.MemberCount, true);
+            embed.AddField("Ban count", (await Context.Guild.GetBansAsync()).Count, true);
             embed.AddField("Roles", Context.Guild.Roles.Count, true);
             embed.WithFooter($"Server ID: {Context.Guild.Id} | Server created: {Context.Guild.CreatedAt.ToString()}");
 
