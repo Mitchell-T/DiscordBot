27c1ef09a8cffd34860d858440524cee0176bbd1
Mitchell Tuinstra <mitchelltuinstra2000@gmail.com>
2019-12-19 02:33:21 +0100

    Basic setup

---
+++ DiscordBotV5/Modules/InfoModule.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace DiscordBot.Modules
+{
+    public class InfoModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("info")]
+        public async Task Info()
+        {
+            await Context.Message.DeleteAsync();
+
+            var embed = new EmbedBuilder();
+            embed.WithTitle("Bot Info");
+            embed.WithColor(Color.Gold);
+            embed.WithDescription($"Hello, My name is {Context.Client.CurrentUser.Username}. I am written in C#(Discord.net) by <@165846891941199872>\n" +
+                "My source code is available at COMING SOON\n");
+
+
+            await Context.Channel.SendMessageAsync("", false, embed.Build());
+        }
+
+        [Command("serverinfo")]
+        public async Task ServerInfo()
+        {
+            var embed = new EmbedBuilder();
+            embed.WithAuthor(Context.Guild.Name, Context.Guild.IconUrl);
+            embed.WithColor(Color.DarkRed);
+            embed.AddField("Owner", Context.Guild.Owner.Mention);
+            embed.AddField("Channels", Context.Guild.Channels.Count, true);
+            embed.AddField("Members", Context.Guild.MemberCount, true);
+            embed.AddField("Roles", Context.Guild.Roles.Count, true);
+            embed.WithFooter($"Server ID: {Context.Guild.Id} | Server created: {Context.Guild.CreatedAt.ToString()}");
+
+
+
+            await Context.Channel.SendMessageAsync("", false, embed.Build());
+        }
+    }
+}
