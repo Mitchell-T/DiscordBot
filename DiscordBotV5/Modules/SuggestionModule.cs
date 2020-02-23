b001145fdd38557096c7f53925cf6058dba49dee
Mitchell Tuinstra <mitchelltuinstra2000@gmail.com>
2020-02-23 23:32:15 +0100

    added suggestion command

---
+++ DiscordBotV5/Modules/SuggestionModule.cs
@@ -0,0 +1,42 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBotV5.Modules
+{
+    public class SuggestionModule : ModuleBase<SocketCommandContext>
+    {
+        private readonly IConfiguration _config;
+
+        public SuggestionModule(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [Command("suggest")]
+        public async Task Suggest([Remainder] string suggestion)
+        {
+            await Context.Message.DeleteAsync();
+
+            SocketTextChannel suggestionChannel = (SocketTextChannel)Context.Guild.GetChannel(Convert.ToUInt64(_config["suggestionChannelID"]));
+
+            IEmote[] emotes = new IEmote[3];
+            emotes[0] = new Emoji("✅");
+            emotes[1] = Emote.Parse("<:neutral:680935638832644193>");
+            emotes[2] = Emote.Parse("<:xmark:680936397313540105>");
+
+            var embed = new EmbedBuilder();
+            embed.WithTitle("New suggestion!");
+            embed.WithDescription(suggestion);
+            embed.WithFooter(DateTime.Now.ToShortDateString() + " || Powered by me");
+            embed.WithColor(new Color(0x03adfc));
+            IUserMessage suggestionMessage = await suggestionChannel.SendMessageAsync("", false, embed.Build());
+            await suggestionMessage.AddReactionsAsync(emotes);
+        }
+    }
+}
