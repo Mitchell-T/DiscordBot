84890e620c36e54207bf8f529fb58343110b8eef
Mitchell Tuinstra <24893@ma-web.nl>
2020-10-16 12:18:05 +0200

    add error checking for quotes and provide 1 overload for retrieving quotes

--- DiscordBotV5/Modules/Fun/QuoteModule.cs
+++ DiscordBotV5/Modules/Fun/QuoteModule.cs
@@ -56,6 +56,8 @@ namespace DiscordBotV5.Modules.Fun
             await m.DeleteAsync();
         }
 
+        
+
         [Command("getQuote")]
         [Alias("q")]
         public async Task GetQuote()
@@ -70,11 +72,71 @@ namespace DiscordBotV5.Modules.Fun
             var filter = new BsonDocument("guild", Context.Guild.Id.ToString());
 
             var results = collection.Find(filter).ToList();
+
+            if (results.Count == 0)
+            {
+                await Context.Channel.SendMessageAsync($"No quotes found for {Context.Guild.Name} \n" +
+                    $"please add some quotes for this user first with `$addquote <messageid>`");
+                return;
+            }
+
             Random rnd = new Random();
             BsonDocument randomQuote = results[rnd.Next(results.Count - 1)];
 
             //string userName = randomQuote.GetValue("userName").ToString();
             //long quotedGuild = randomQuote.GetValue("guild").ToInt64();
+            string quotedText = randomQuote.GetValue("quote").ToString();
+            long quoteduser = randomQuote.GetValue("userID").ToInt64();
+            DateTime date = (DateTime)randomQuote.GetValue("date");
+
+            IGuildUser messageAuthor = Context.Guild.GetUser((ulong)quoteduser);
+
+            var embed = new EmbedBuilder();
+            embed.WithAuthor(messageAuthor);
+            embed.WithColor(new Color(rnd.Next(255), rnd.Next(255), rnd.Next(255)));
+            embed.WithTimestamp(date);
+            embed.WithDescription($"{quotedText} - **{date.Year}**");
+
+            await Context.Channel.SendMessageAsync("", false, embed.Build());
+
+
+        }
+
+        [Command("getQuote")]
+        [Alias("q")]
+        public async Task GetQuote(IGuildUser user)
+        {
+            await Context.Message.DeleteAsync();
+
+            // Connect to database and get collection
+            MongoClient dbClient = new MongoClient($"mongodb://{_config["dbUsername"]}:{_config["dbPassword"]}@{_config["dbAddress"]}:{_config["dbPort"]}/?authSource=admin&readPreference=primary");
+            IMongoDatabase database = dbClient.GetDatabase("DiscordBot");
+            IMongoCollection<BsonDocument> collection = database.GetCollection<BsonDocument>("Quotes");
+
+            var filter = new BsonDocument("guild", Context.Guild.Id.ToString());
+
+            var results = collection.Find(filter).ToList();
+            Console.WriteLine(results.Count);
+
+            List<BsonDocument> actualResults = new List<BsonDocument>();
+
+            foreach (var result in results)
+            {
+                if (result.GetValue("userID").ToInt64() == (long)user.Id)
+                {
+                    actualResults.Add(result);
+                }
+            }
+            if(actualResults.Count == 0)
+            {
+                await Context.Channel.SendMessageAsync($"No quotes found for {user.Mention} \n" +
+                    $"please add some quotes for this user first with `$addquote <messageid>`");
+                return;
+            }
+
+            Random rnd = new Random();
+            BsonDocument randomQuote = actualResults[rnd.Next(actualResults.Count - 1)];
+
             string quotedText = randomQuote.GetValue("quote").ToString();
             long quoteduser = randomQuote.GetValue("userID").ToInt64();
             DateTime date = (DateTime)randomQuote.GetValue("date");
