a801886f8743c546187152f1d8586172b25a465c
Mitchell-T <mitchell@mitchelltuinstra.nl>
2020-10-28 01:59:13 +0100

    started implementing Victoria

--- DiscordBotV5/Modules/Audio/AudioModule.cs
+++ DiscordBotV5/Modules/Audio/AudioModule.cs
@@ -1,35 +1,155 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using Victoria;
+using Victoria.Enums;
+using Victoria.EventArgs;
 
 public class AudioModule : ModuleBase<SocketCommandContext>
 {
-    private readonly AudioService _service;
+    private readonly LavaNode _lavaNode;
 
-    public AudioModule(AudioService service)
+    public AudioModule(IServiceProvider provider)
     {
-        _service = service;
+        _lavaNode = provider.GetRequiredService<LavaNode>();
+        _lavaNode.OnTrackEnded += OnTrackEnded;
     }
 
-    [Command("join", RunMode = RunMode.Async)]
-    [Summary("Join your current voice channel")]
-    public async Task JoinCmd()
+    [Command("Join")]
+    public async Task JoinAsync()
     {
-        await _service.JoinAudio(Context.Guild, (Context.User as IVoiceState).VoiceChannel);
+        if (_lavaNode.HasPlayer(Context.Guild))
+        {
+            await ReplyAsync("I'm already connected to a voice channel!");
+            return;
+        }
+
+        var voiceState = Context.User as IVoiceState;
+        if (voiceState?.VoiceChannel == null)
+        {
+            await ReplyAsync("You must be connected to a voice channel!");
+            return;
+        }
+
+        try
+        {
+            await _lavaNode.JoinAsync(voiceState.VoiceChannel, Context.Channel as ITextChannel);
+            await ReplyAsync($"Joined {voiceState.VoiceChannel.Name}!");
+        }
+        catch (Exception exception)
+        {
+            await ReplyAsync(exception.Message);
+        }
     }
 
     [Command("leave", RunMode = RunMode.Async)]
     [Summary("Leave the voice channel")]
     public async Task Leave()
     {
-        await _service.LeaveAudio(Context.Guild);
+        var player = _lavaNode.GetPlayer(Context.Guild);
+        await _lavaNode.LeaveAsync(player.VoiceChannel);
     }
 
-    [Command("play", RunMode = RunMode.Async)]
-    [Summary("Play audio from a youtube url")]
-    public async Task Play([Remainder] string song)
+    [Command("Play")]
+    public async Task PlayAsync([Remainder] string searchQuery)
     {
-        await _service.JoinAudio(Context.Guild, (Context.User as IVoiceState).VoiceChannel);
-        await _service.SendAudioAsync(Context.Guild, Context.Channel, song);
+
+        if (string.IsNullOrWhiteSpace(searchQuery))
+        {
+            await ReplyAsync("Please provide search terms.");
+            return;
+        }
+
+        if (!_lavaNode.HasPlayer(Context.Guild))
+        {
+            await JoinAsync();
+        }
+
+        var searchResponse = await _lavaNode.SearchYouTubeAsync(searchQuery);
+        if (searchResponse.LoadStatus == LoadStatus.LoadFailed ||
+            searchResponse.LoadStatus == LoadStatus.NoMatches)
+        {
+            await ReplyAsync($"I wasn't able to find anything for `{searchQuery}`.");
+            return;
+        }
+
+        var player = _lavaNode.GetPlayer(Context.Guild);
+
+        if (player.PlayerState == PlayerState.Playing || player.PlayerState == PlayerState.Paused)
+        {
+            if (!string.IsNullOrWhiteSpace(searchResponse.Playlist.Name))
+            {
+                foreach (var track in searchResponse.Tracks)
+                {
+                    player.Queue.Enqueue(track);
+                }
+
+                await ReplyAsync($"Enqueued {searchResponse.Tracks.Count} tracks.");
+            }
+            else
+            {
+                var track = searchResponse.Tracks[0];
+                player.Queue.Enqueue(track);
+                await ReplyAsync($"Enqueued: {track.Title}");
+            }
+        }
+        else
+        {
+            var track = searchResponse.Tracks[0];
+
+            if (!string.IsNullOrWhiteSpace(searchResponse.Playlist.Name))
+            {
+                for (var i = 0; i < searchResponse.Tracks.Count; i++)
+                {
+                    if (i == 0)
+                    {
+                        await player.PlayAsync(track);
+                        await ReplyAsync($"Now Playing: ({track.Title})[{track.Url}]");
+                    }
+                    else
+                    {
+                        player.Queue.Enqueue(searchResponse.Tracks[i]);
+                    }
+                }
+
+                await ReplyAsync($"Enqueued {searchResponse.Tracks.Count} tracks.");
+            }
+            else
+            {
+                await player.PlayAsync(track);
+                //await ReplyAsync($"Now Playing: ({track.Title})[{track.Url}]");
+                EmbedBuilder embed = new EmbedBuilder();
+                embed.WithAuthor(Context.User);
+                embed.WithTitle("♪Now Playing♪");
+                embed.WithDescription($"Now Playing: ({track.Title})[{track.Url}]");
+                await ReplyAsync("", false, embed.Build());
+            }
+        }
+    }
+
+    private async Task OnTrackEnded(TrackEndedEventArgs args)
+    {
+        if (!args.Reason.ShouldPlayNext())
+        {
+            return;
+        }
+
+        var player = args.Player;
+        if (!player.Queue.TryDequeue(out var queueable))
+        {
+            await player.TextChannel.SendMessageAsync("Queue completed! Please add more tracks to rock n' roll!");
+            return;
+        }
+
+        if (!(queueable is LavaTrack track))
+        {
+            await player.TextChannel.SendMessageAsync("Next item in queue is not a track.");
+            return;
+        }
+
+        await args.Player.PlayAsync(track);
+        await args.Player.TextChannel.SendMessageAsync($"{args.Reason}: {args.Track.Title}\nNow playing: {track.Title}");
     }
 }
